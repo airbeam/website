57646b2c39054432d65bbc51a5248c1cb01f78e8
Jonathon <jfritz50@gmail.com>
2019-03-14 11:48:13 -0700

    update

--- components/Home.js
+++ components/Home.js
@@ -1,25 +1,38 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { css } from "emotion";
-function Home() {
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+function Home({ style }) {
   return (
     <div>
       <Header />
-
-      <div
-        className={css`
-          margin-top: 90px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        `}
+      <CarouselProvider
+      isPlaying
+        naturalSlideWidth={10}
+        naturalSlideHeight={125}
+        totalSlides={3}
       >
-        <h3>
-          This site is under construction, email inquiries to: 
-
-          {'  '}<a href="mailto:sales@airbeamtec.com">sales@airbeamtec.com</a>
-        </h3>
-      </div>
+        <Slider>
+          <Slide index={0}>
+            <img src="/static/carousel_a.png" alt="my image" />
+          </Slide>
+          <Slide index={1}>
+            <img src="/static/carousel_b.png" alt="my image" />
+          </Slide>
+          <Slide index={2}>
+            <img src="/static/carousel_c.png" alt="my image" />
+          </Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider>
 
       <Footer />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Home from "../components/Home";
-
 function Index() {
-  return <Home />;
+  return(
+  
+      <Home />
+  )
+  
+
 }
 
 export default Index;
