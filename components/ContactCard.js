43a68de0d0b5e5bb543d558912a30cca05bd1d37
John Fritz <johnfritz@Johns-Mac-mini.local>
2019-04-01 20:54:07 -0700

    update

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,65 +1,65 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import MenuItem from "@material-ui/core/MenuItem";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import { css } from "emotion";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { css } from 'emotion';
 
-const styles = theme => ({
-  container: {
-    display: "flex",
-    flexWrap: "wrap"
+const styles = (theme) => ({
+  container : {
+    display  : 'flex',
+    flexWrap : 'wrap'
   },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit
+  textField : {
+    marginLeft  : theme.spacing.unit,
+    marginRight : theme.spacing.unit
   },
-  dense: {
-    marginTop: 16
+  dense     : {
+    marginTop : 16
   },
-  menu: {
-    width: 200
+  menu      : {
+    width : 200
   },
-  card: {
-    marginLeft: 200
+  card      : {
+    marginLeft : 200
     // marginTop  : 30
     //  maxWidth   : '30%'
     // width: 100
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
+  bullet    : {
+    display   : 'inline-block',
+    margin    : '0 2px',
+    transform : 'scale(0.8)'
   },
-  title: {
-    fontSize: 24
+  title     : {
+    fontSize : 24
   },
-  pos: {
-    marginBottom: 12
+  pos       : {
+    marginBottom : 12
   },
-  container: {
-    display: "flex",
-    flexWrap: "wrap"
+  container : {
+    display  : 'flex',
+    flexWrap : 'wrap'
   },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit
+  textField : {
+    marginLeft  : theme.spacing.unit,
+    marginRight : theme.spacing.unit
   },
-  dense: {
-    marginTop: 16
+  dense     : {
+    marginTop : 16
   },
-  menu: {
-    width: 200
+  menu      : {
+    width : 200
   }
 });
 
@@ -87,19 +87,39 @@ function SimpleCard(props) {
         Canada
       </Typography>
       <br />
-      <Typography color="textPrimary">Phone:(778)338-8047</Typography>
-      {/**
-            <Typography color="textPrimary">Phone: +1 (416) 637-1048</Typography>
-      
-      */}
+      <a
+        className={css`
+          color: #6b6b6d;
+          font-weight: bold;
+          font: outline;
+          text-decoration: none;
+        `}
+        href="tel:+17783388047"
+        target="_top"
+      >
+        Phone:+1-778-338-8047
+      </a>
+ 
       <br />
-      <Typography color="primary">sales@airbeamtech.com</Typography>
+      <br />
+      <a
+        className={css`
+          color: #6b6b6d;
+          font-weight: bold;
+          font: outline;
+          text-decoration: none;
+        `}
+        href="mailto:sales@airbeamtech.com?Subject=Sales%20inquiry"
+        target="_top"
+      >
+        sales@airbeamtech.com
+      </a>
     </div>
   );
 }
 
 SimpleCard.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes : PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(SimpleCard);
