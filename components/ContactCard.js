8cdb0761fbb6bc54728673171d707d0b6dcb37dc
Jonathon <jfritz50@gmail.com>
2019-04-13 11:02:16 -0700

    update

--- components/ContactCard.js
+++ components/ContactCard.js
@@ -1,67 +1,66 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { css } from 'emotion';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { css } from "emotion";
 
-const styles = (theme) => ({
-  container : {
-    display  : 'flex',
-    flexWrap : 'wrap'
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
   },
-  textField : {
-    marginLeft  : theme.spacing.unit,
-    marginRight : theme.spacing.unit
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
   },
-  dense     : {
-    marginTop : 16
+  dense: {
+    marginTop: 16
   },
-  menu      : {
-    width : 200
+  menu: {
+    width: 200
   },
-  card      : {
-    marginLeft : 200
+  card: {
+    marginLeft: 200
     // marginTop  : 30
     //  maxWidth   : '30%'
     // width: 100
   },
-  bullet    : {
-    display   : 'inline-block',
-    margin    : '0 2px',
-    transform : 'scale(0.8)'
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
   },
-  title     : {
-    fontSize : 24,
-    fontFamily: 'CircularAirPro-Book'
-
+  title: {
+    fontSize: 24,
+    fontFamily: "CircularAirPro-Book"
   },
-  pos       : {
-    marginBottom : 12
+  pos: {
+    marginBottom: 12
   },
-  container : {
-    display  : 'flex',
-    flexWrap : 'wrap'
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
   },
-  textField : {
-    marginLeft  : theme.spacing.unit,
-    marginRight : theme.spacing.unit
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
   },
-  dense     : {
-    marginTop : 16
+  dense: {
+    marginTop: 16
   },
-  menu      : {
-    width : 200
+  menu: {
+    width: 200
   }
 });
 
@@ -75,7 +74,6 @@ function SimpleCard(props) {
         @media (max-width: 780px) {
           margin-top: 20px;
           margin-left: 120px;
-
         }
       `}
     >
@@ -84,12 +82,11 @@ function SimpleCard(props) {
       </Typography>
       <br />
       <Typography
-      
-      className={css`
-          font-family: 'CircularAirPro-Light';
-
-      `}
-      color="textPrimary">
+        className={css`
+          font-family: "CircularAirPro-Light";
+        `}
+        color="textPrimary"
+      >
         21320 Gordon Way #125
         <br />
         Richmond, BC V6W 1J8 <br />
@@ -102,20 +99,19 @@ function SimpleCard(props) {
           font-weight: bold;
           font: outline;
           text-decoration: none;
-          font-family: 'CircularAirPro-Light';
-
+          font-family: "CircularAirPro-Light";
         `}
         href="tel:+17783388047"
         target="_top"
       >
         Phone: +1 (778) 338-8047
       </a>
- 
+
       <br />
       <br />
       <a
         className={css`
-          font-family: 'CircularAirPro-Light';
+          font-family: "CircularAirPro-Light";
           color: #6b6b6d;
           font-weight: bold;
           font: outline;
@@ -131,7 +127,7 @@ function SimpleCard(props) {
 }
 
 SimpleCard.propTypes = {
-  classes : PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(SimpleCard);
